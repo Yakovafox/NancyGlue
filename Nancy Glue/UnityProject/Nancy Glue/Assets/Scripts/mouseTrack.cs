50e37e03a92f6f5b1063cf14b8822e90b7f89564
randal22 <55952795+randal22@users.noreply.github.com>
2023-02-21 16:02:53 +0000

    object tags setup

--- Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/mouseTrack.cs
+++ Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/mouseTrack.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //simple script to check mouse over any 3d game object collider 
+//need to add 
 
 public class mouseTrack : MonoBehaviour
 {
@@ -11,16 +12,33 @@ public class mouseTrack : MonoBehaviour
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitData;
         //Vector3 start = new Vector3(0, 1, -10);
         //Vector3 dir = new Vector3(0, 0, 1);
         //Debug.DrawRay(start, dir, Color.blue);
-        RaycastHit hitData;
+
         if (Input.GetMouseButtonDown(0))
         {
             if (Physics.Raycast(ray, out hitData, 1000))
             {
                 worldPos = hitData.point;
-                Debug.Log("object hit");
+
+
+                switch (hitData.transform.tag)
+                {
+                    case ("Evidence"):
+                        Debug.Log("Clicked evidence");
+                        break;
+                    case ("NPC"):
+                        Debug.Log("Clicked NPC");
+                        break;
+                    case ("Interactable"):
+                        Debug.Log("Clicked Interactable");
+                        break;
+                    case ("Untagged"):
+                        Debug.Log("Other object hit");
+                        break;
+                }
             }
         }
     }
