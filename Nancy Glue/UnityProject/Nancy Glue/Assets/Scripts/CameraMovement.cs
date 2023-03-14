976c050dbf976d52bf9921dbb991340dbf3833af
RYAN MILLER <2103251@uad.ac.uk>
2023-03-14 12:48:48 +0000

    Keyboard control implemented
    - Speed value added.
    - Input added, allows for mouse or keyboard controls for turning camera.
    - refactored angle clamp method.

--- Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/CameraMovement.cs
+++ Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/CameraMovement.cs
@@ -7,23 +7,21 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private Transform _rootCamera; //transform for the root camera.
     [SerializeField] private Transform _cameraTransform; //transform for the current camera.
     [SerializeField] private List<Transform> _switchableCameras; //transforms for cameras that can be switched to.
-    [SerializeField] [Range(0,45)]private float _yRotLimit;
-    [SerializeField] [Range(0,45)]private float _xRotLimit;
+    [SerializeField] [Range(0,45)]private float _yRotLimit; //Limit rotation on the Y Axis
+    [SerializeField] [Range(0,45)]private float _xRotLimit; //Limit Rotation on the X Axis
+    [SerializeField] [Range(20,50)]private float _turnSpeed;
     [SerializeField] private Vector3 _mousePos;
     [SerializeField] private Vector2 _screenBounds;
-    [SerializeField][Range(0, 100)] private int _screenBuffer;
+    [SerializeField][Range(50, 100)] private int _screenBuffer = 100;
     [SerializeField] private bool _lookLeft, _lookRight, _lookUp, _lookDown;
     [SerializeField] private Vector3 _initialRotation;
-    [SerializeField] private float _cameraXMin, _cameraXMax, _parentYMin, _parentYMax;
-    [SerializeField] private Transform _parent;
-    [SerializeField] private Vector3 _parentRotation;
+    [SerializeField] private float _cameraXMin, _cameraXMax, _cameraYMin, _cameraYMax;
     [SerializeField] private Transform _hitBoxTransform;
     [SerializeField] private float _angleX, _angleY;
 
     private void Awake()
     {
         _cameraTransform = GetComponent<Transform>();
-        _parent = _cameraTransform.parent;
     }
 
     // Start is called before the first frame update
@@ -31,12 +29,11 @@ public class CameraMovement : MonoBehaviour
     {
         _screenBounds = new Vector2(Screen.width - _screenBuffer, Screen.height - _screenBuffer);
         _isRoot = _rootCamera == null;
-        _parentRotation = _parent.eulerAngles;
-        _initialRotation = new Vector3(_cameraTransform.rotation.eulerAngles.x, _parent.rotation.eulerAngles.y, 0);
+        _initialRotation = new Vector3(_cameraTransform.rotation.eulerAngles.x, _cameraTransform.rotation.eulerAngles.y, 0);
         _cameraXMin = _initialRotation.x - _xRotLimit;
         _cameraXMax = _initialRotation.x + _xRotLimit;
-        _parentYMin = _initialRotation.y - _yRotLimit;
-        _parentYMax = _initialRotation.y + _yRotLimit;
+        _cameraYMin = _initialRotation.y - _yRotLimit;
+        _cameraYMax = _initialRotation.y + _yRotLimit;
         _angleX = _initialRotation.x;
         _angleY = _initialRotation.y;
     }
@@ -50,10 +47,10 @@ public class CameraMovement : MonoBehaviour
     private void CheckBounds()
     {
         _mousePos = Input.mousePosition;
-        _lookLeft = _mousePos.x < _screenBuffer ? true : false;
-        _lookRight = _mousePos.x > _screenBounds.x ? true : false;
-        _lookUp = _mousePos.y < _screenBuffer ? true : false;
-        _lookDown = _mousePos.y > _screenBounds.y ? true : false;
+        _lookLeft = _mousePos.x < _screenBuffer || Input.GetKey(KeyCode.A) ? true : false;
+        _lookRight = _mousePos.x > _screenBounds.x || Input.GetKey(KeyCode.D) ? true : false;
+        _lookUp = _mousePos.y > _screenBounds.y || Input.GetKey(KeyCode.W) ? true : false;
+        _lookDown =  _mousePos.y < _screenBuffer || Input.GetKey(KeyCode.S) ? true : false;
         RotateLeftRight();
         RotateUpDown();
     }
@@ -80,10 +77,10 @@ public class CameraMovement : MonoBehaviour
         switch (_lookUp)
         {
             case true:
-                Rotate(1, 0);
+                Rotate(-1, 0);
                 break;
             case false when _lookDown:
-                Rotate(-1, 0);
+                Rotate(1, 0);
                 break;
             case false:
                 break;
@@ -93,23 +90,17 @@ public class CameraMovement : MonoBehaviour
     private void Rotate(int x, int y)
     {
         if (x != 0)
-        {
-            _angleX += (x * 10f) * Time.deltaTime;
-            _angleX = Mathf.Clamp(_angleX, _cameraXMin, _cameraXMax);
-        }
+            _angleX = AngleClamp(x, _angleX, _cameraXMin, _cameraXMax);
+        
         if (y != 0)
-        { 
-            _angleY += (y * 10f) * Time.deltaTime;
-            _angleY = Mathf.Clamp(_angleY, _parentYMin, _parentYMax);
-        }
-        _parent.eulerAngles = new Vector3(0, _angleY, 0);
-        _cameraTransform.eulerAngles = new Vector3(_angleX, _angleY, 0); //don't know why this needs angleX and angleY, but camera will not rotate left or right without angleY.
+            _angleY = AngleClamp(y, _angleY, _cameraYMin, _cameraYMax);
+        _cameraTransform.eulerAngles = new Vector3(_angleX, _angleY, 0);
     }
 
-    private float AngleClamp(float angle, float AxisRotationMin, float AxisRotationMax, int Direction)
+    private float AngleClamp(int axisDirection, float axisAngle, float clampMin, float clampMax)
     {
-        angle += Direction * Time.deltaTime;
-        angle = Mathf.Clamp(angle, AxisRotationMin, AxisRotationMax);
-        return angle;
+        axisAngle += (axisDirection * _turnSpeed) * Time.deltaTime;
+        axisAngle = Mathf.Clamp(axisAngle, clampMin, clampMax);
+        return axisAngle;
     }
 }
