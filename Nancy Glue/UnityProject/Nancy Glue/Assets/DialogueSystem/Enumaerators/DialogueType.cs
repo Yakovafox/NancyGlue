ba6965a4ceffca3fdcd7a251a2d7705aa81b7dd9
JonathanTee <1900172@uad.ac.uk>
2023-03-15 15:30:47 +0000

    Location Node Added

--- Nancy Glue/UnityProject/Nancy Glue/Assets/DialogueSystem/Enumaerators/DialogueType.cs
+++ Nancy Glue/UnityProject/Nancy Glue/Assets/DialogueSystem/Enumaerators/DialogueType.cs
@@ -9,6 +9,7 @@ namespace Dialogue.Enumerators
     {
         SingleChoice,       // Single for if there's a progression to single box
         MultiChoice,        // Multi for if there's a dialogue tree
-        Evidence            // Evidence for if it should be recorded
+        Evidence,           // Evidence for if it should be recorded
+        Location            // Location for if it should be explored
     }
 }
