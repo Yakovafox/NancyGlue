00559ee8cf9af3093db9b673060242b1575e5198
Ryan Miller <2103251@uad.ac.uk>
2023-03-20 13:48:50 +0000

    Update ItemData.cs

--- Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/Item Scripts/ItemData.cs
+++ Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/Item Scripts/ItemData.cs
@@ -6,9 +6,4 @@ public class ItemData : MonoBehaviour
 {
     [SerializeField] private ItemScriptableObject _evidenceItem;
     public ItemScriptableObject EvidenceItem => _evidenceItem;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 }
