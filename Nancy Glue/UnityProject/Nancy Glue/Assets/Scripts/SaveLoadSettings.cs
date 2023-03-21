d2d41630b8cf60dcea7498c9f0b859b469e618ec
randal22 <55952795+randal22@users.noreply.github.com>
2023-03-21 15:00:50 +0000

    prep

--- Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/SaveLoadSettings.cs
+++ Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/SaveLoadSettings.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveLoadSettings : MonoBehaviour
 {
+
+    public int sfxVolume;
+    public int musicVolume;
+    public Color fontColour;
+    public int sensitivity;
+    public Color backgroundColour;
+    public string fontStyle;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +25,32 @@ public class SaveLoadSettings : MonoBehaviour
     {
         
     }
+
+    public void save()
+    {
+        PlayerPrefs.SetInt("sfxVolume", sfxVolume);
+        PlayerPrefs.SetInt("musicVolume",musicVolume);
+        PlayerPrefs.SetString("fontColour", ColorUtility.ToHtmlStringRGB(fontColour));
+        PlayerPrefs.SetString("fontStyle", fontStyle);
+        PlayerPrefs.SetString("backgroundColour", ColorUtility.ToHtmlStringRGB(backgroundColour));
+        PlayerPrefs.SetInt("sensitivity", sensitivity);
+        PlayerPrefs.Save();
+        Debug.Log("saved to");//write path later
+    }
+    public void load()
+    {
+        sfxVolume = PlayerPrefs.GetInt("sfxVolume");
+        musicVolume = PlayerPrefs.GetInt("musicVolume");
+        ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("fontColour"), out fontColour);
+        
+        fontStyle = PlayerPrefs.GetString("fontStyle");
+        
+        ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("backgroundColour"),out backgroundColour);
+        sensitivity = PlayerPrefs.GetInt("sensitivity");
+    }
+    public void Default()
+    { 
+        //revert to default settings
+    }
+
 }
