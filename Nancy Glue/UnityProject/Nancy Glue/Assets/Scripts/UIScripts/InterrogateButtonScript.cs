b7726c1a0033b7b24c1e5777e269019f91353e8d
JonathanTee <1900172@uad.ac.uk>
2023-04-19 15:39:06 +0100

    Update UI.prefab

--- Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/UIScripts/InterrogateButtonScript.cs
+++ Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/UIScripts/InterrogateButtonScript.cs
@@ -3,11 +3,13 @@ using Dialogue;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InterrogateButtonScript : MonoBehaviour
 {
     // NPC being questioned
     private NPCTracker NPC;
+    [SerializeField] private Button button;
 
     // Dialogue
     private DialogueSystem dialogue;
@@ -22,6 +24,7 @@ public class InterrogateButtonScript : MonoBehaviour
         _transitionAnimator = GameObject.Find("Transition").GetComponent<Animator>();
         _uiAnimator = GameObject.Find("ClipBoardBack").GetComponent<Animator>();
         dialogue = FindObjectOfType<DialogueSystem>(true);
+        //button.onClick.AddListener(BringForInterrogation);
     }
 
     public void SetNpc(NPCTracker npc)
@@ -31,22 +34,27 @@ public class InterrogateButtonScript : MonoBehaviour
 
     public void BringForInterrogation()
     {
-        _uiAnimator.gameObject.GetComponent<OpenCloseUI>().CloseUI();
+        //button.setAc
         ZoneManager _zoneManager = FindObjectOfType<ZoneManager>();
         StartCoroutine(ZoneTransition(_zoneManager.CurrentCamera, _zoneManager.OfficeCam));
+        _uiAnimator.gameObject.GetComponent<OpenCloseUI>().CloseUI();
     }
 
     IEnumerator ZoneTransition(CameraSwitch oldCam, CameraSwitch newCam)
     {
-       
         _transitionAnimator.SetTrigger("FadeIn");
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(1);
         oldCam.SwitchActiveCam();
         newCam.SwitchActiveCam();
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(1);
+        Debug.LogError("Transtion");
         GameObject.Find(NPC.attachedNPC).transform.position = GameObject.Find("SeatLocation").transform.position;
+        Debug.LogError("Move NPC");
         _uiAnimator.SetBool("hide", true);
+        Debug.LogError("Hide UI");
         dialogue.SetContainer(NPC.GetCurrentInterContainer(), NPC);
         _transitionAnimator.SetTrigger("FadeOut");
+        Debug.LogError("Fade");
+        yield return new WaitForSeconds(0);
     }
 }
