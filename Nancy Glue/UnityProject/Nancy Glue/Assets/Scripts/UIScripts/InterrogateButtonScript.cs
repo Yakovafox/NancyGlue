76853d138896cd50de10f952c20a1766e64dcb99
RYAN MILLER <2103251@uad.ac.uk>
2023-05-02 11:12:03 +0100

    scene changes
    -changed cursor settings to get rid of warnings
    -removed Igor's hat and glasses disguise.

--- Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/UIScripts/InterrogateButtonScript.cs
+++ Nancy Glue/UnityProject/Nancy Glue/Assets/Scripts/UIScripts/InterrogateButtonScript.cs
@@ -33,16 +33,16 @@ public class InterrogateButtonScript : MonoBehaviour
 
     public void BringForInterrogation()
     {
-        ZoneManager _zoneManager = FindObjectOfType<ZoneManager>();
-        StartCoroutine(ZoneTransition(_zoneManager.CurrentCamera, _zoneManager.OfficeCam));
-        AudioSource audioSource = GameObject.Find("Player").GetComponents<AudioSource>()[0];
-        audioSource.clip = Resources.Load<AudioClip>("Sfx/Music/Interrogation_Music");
-        audioSource.Play();
         var locationbuttons = FindObjectsOfType<LocationButton>();
         foreach(var button in locationbuttons)
         {
             button.LocationCheck();
         }
+        ZoneManager _zoneManager = FindObjectOfType<ZoneManager>();
+        StartCoroutine(ZoneTransition(_zoneManager.CurrentCamera, _zoneManager.OfficeCam));
+        AudioSource audioSource = GameObject.Find("Player").GetComponents<AudioSource>()[0];
+        audioSource.clip = Resources.Load<AudioClip>("Sfx/Music/Interrogation_Music");
+        audioSource.Play();
     }
 
     IEnumerator ZoneTransition(CameraSwitch oldCam, CameraSwitch newCam)
